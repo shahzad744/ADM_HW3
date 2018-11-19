6663d6f5c81cf213303f1464ae5f532be81a79ac
Shahzad Amin <shahzad.744@gmail.com>
2018-11-19 17:10:54 +0100

    scored text mining fixes

--- TextMining.py
+++ TextMining.py
@@ -3,16 +3,18 @@ import heapq
 import math
 from collections import defaultdict
 class TextMining:
-    __k=20
+    __k=100      
 
-    def __calculateCosineSimilarity(self,listOfScores):
+    def __calculateCosineSimilarity(self,listOfScores,queryscoreList):
         sum=0
-        squaredsum=0
-        for scores in listOfScores:
-            sum+=scores
-            squaredsum+= scores*scores
-        val=sum/math.sqrt(squaredsum)
-        return val/len(listOfScores)
+        squaredsumA=0
+        squaredsumB=0
+        for i in range(0,len(queryscoreList)):
+            sum+=listOfScores[i]*queryscoreList[i]
+            squaredsumA+= listOfScores[i]*listOfScores[i]
+            squaredsumB+= queryscoreList[i]*queryscoreList[i]
+        val=sum/(math.sqrt(squaredsumA)*math.sqrt(squaredsumB))
+        return val
     #it takes inverted table and search query and perform text mining on
     #inverted table and return list of search results
     def SearchTextFromInvertedIndexAndReturnResults(self,invertedTable,searchText):
@@ -33,13 +35,22 @@ class TextMining:
         documentListWithScores=[]
         documentListWithScoresSorted=[]
         vocab=WordsDictionary.WordsDictionary()
-        termScores=defaultdict(list)        
+        termScores=defaultdict(list)  
+        totalDocuments=set()
+        vals=list(invertedScoredTable.values())
+        for i in vals:
+            for j in i:
+                totalDocuments.add(j[0])
+        querytermScores=[]
         for searchWord in searchText:
             if(vocab.HasWordInDictionary(searchWord)):
                 wordIndex=vocab.GetTermIdByWord(searchWord)
                 if(str(wordIndex) in invertedScoredTable):
                     docs=invertedScoredTable[str(wordIndex)]
                     docsSet=set()
+                    tf=searchText.count(searchWord)/len(searchText)
+                    idf=math.log2(len(totalDocuments)/len(docs))
+                    querytermScores.append(tf*idf)
                     for doc in docs:
                         docsSet.add(doc[0])
                         termScores[doc[0]].append(doc[1])
@@ -49,7 +60,7 @@ class TextMining:
 
         listOfDocs=list(set.intersection(*listOfDocs))
         for dd in listOfDocs:
-            heapq.heappush(documentListWithScores,(1-self.__calculateCosineSimilarity(termScores[dd]),dd))
+            heapq.heappush(documentListWithScores,(1-self.__calculateCosineSimilarity(termScores[dd],querytermScores),dd))
         i=0
         while i <self.__k and i<len(documentListWithScores):
             temp=heapq.heappop(documentListWithScores)
